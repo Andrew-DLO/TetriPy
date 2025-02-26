7b652c81b649468c484fb522bc7d4cbe5dbf7ff1
Andrew-DLO <andrewdelaossa04@gmail.com>
2025-02-25 23:59:14 -0800

    Added pygame setup and settings variables

--- main.py
+++ main.py
@@ -8,4 +8,25 @@ class App:
         self.screen = pg.display.set_mode(PLAYAREA_RES)
         self.clock = pg.time.Clock()
 
-    
+    def update(self):
+        self.clock.tick(FPS)
+
+    def draw(self):
+        self.screen.fill(color=PLAYAREA_COLOR)
+        pg.display.flip()
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.type == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.update()
+            self.draw()
+
+if __name__ == '__main__':
+    app = App()
+    app.run()
